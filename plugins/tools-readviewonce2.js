1400fbc178ba8a41fa64529f208a49bfa6495c9f
GDS <112704679+DASTAGHIR@users.noreply.github.com>
2025-01-16 17:10:26 +0500

    Update

--- plugins/tools-readviewonce2.js
+++ plugins/tools-readviewonce2.js
@@ -1,15 +1,29 @@
-let { downloadContentFromMessage } = (await import('@whiskeysockets/baileys'));
+import { downloadContentFromMessage } from '@whiskeysockets/baileys';
 
 var handler = async (m, { conn }) => {
-    if (!/viewOnce/.test(m.quoted?.mtype)) throw '✳️❇️ Its Not a ViewOnce Message'
- let mtype = Object.keys(m.quoted.message)[0]
- let buffer = await m.quoted.download()
- let caption = m.quoted.message[mtype].caption || ''
- conn.sendMessage(conn.user.id, { [mtype.replace(/Message/, '')]: buffer, caption }, { quoted: m })
-}
+    if (!m.quoted || !/viewOnce/.test(m.quoted.mtype)) {
+        throw '✳️❇️ Its Not a ViewOnce Message';
+    }
+    
+    try {
+        let mtype = Object.keys(m.quoted.message)[0];
+        let stream = await downloadContentFromMessage(m.quoted.message[mtype], mtype.replace('Message', ''));
+        let buffer = Buffer.concat([]);
+        
+        for await (const chunk of stream) {
+            buffer = Buffer.concat([buffer, chunk]);
+        }
+        
+        let caption = m.quoted.message[mtype].caption || '';
+        await conn.sendMessage(conn.user.id, { [mtype.replace(/Message/, '')]: buffer, caption }, { quoted: m });
+    } catch (error) {
+        console.error(error);
+        throw 'Failed to process the view-once message.';
+    }
+};
 
-handler.help = ['read2', 'v']
-handler.tags = ['tools']
-handler.command = ['readviewonce2', 'read2', 'v'] 
+handler.help = ['read2'];
+handler.tags = ['tools'];
+handler.command = ['readviewonce2', 'read2', 'v', 'readvo2'];
 
-export default handler
+export default handler;
